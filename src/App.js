e6247e8f16e6274d4cc4b5112ac358edb2cfe414
FatimaBadran97 <50345208+FatimaBadran97@users.noreply.github.com>
2021-10-27 17:41:40 +0300

    Add files via uploads

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// jshint esversion:6
+
+import React from 'react';
+
+import CharacterEditor from './components/CharacterEditor';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
